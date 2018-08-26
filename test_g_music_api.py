a3c5d07569ede7c718cca2e38a022f27d8316bdd
mark guindi <>
2018-08-25 22:48:08 -0400

    Refactored to make less time-consuming api calls

--- test_g_music_api.py
+++ test_g_music_api.py
@@ -11,7 +11,7 @@ import secret
 api = Mobileclient()
 api.login(secret.USERNAME, secret.PASSWORD, Mobileclient.FROM_MAC_ADDRESS)
 
-playlists = api.get_all_user_playlist_contents()
+playlists = api.get_all_playlists()
 
 # Gets the playlists we are working with: The playlist with all the songs, the
 # playlist that will hold uncategorized songs, and the playlists for the two
@@ -19,37 +19,44 @@ playlists = api.get_all_user_playlist_contents()
 playlists_to_use = [p for p in playlists if p['name'] in (secret.BIG_PLAYLIST, secret.UNCATEGORIZED, secret.CATEGORY_A, secret.CATEGORY_B)]
 
 # It is valid to assume that the playlist which holds ALL songs already exists
-big_playlist = [p for p in playlists_to_use if p['name'] == secret.BIG_PLAYLIST]
-big_playlist = big_playlist[0]
-
-uncategorized = [p for p in playlists_to_use if p['name'] == secret.UNCATEGORIZED]
-category_a = [p for p in playlists_to_use if p['name'] == secret.CATEGORY_A]
-category_b = [p for p in playlists_to_use if p['name'] == secret.CATEGORY_B]
+big_playlist = [p for p in playlists_to_use if p['name'] == secret.BIG_PLAYLIST][0]
+uncategorized_lst = [p for p in playlists_to_use if p['name'] == secret.UNCATEGORIZED]
+category_a_lst = [p for p in playlists_to_use if p['name'] == secret.CATEGORY_A]
+category_b_lst = [p for p in playlists_to_use if p['name'] == secret.CATEGORY_B]
 
 # If "Uncategorized" playlist already exists, we want it empty so we can
 # populate it with new uncategorized songs, and the easient way to do that is to
 # delete it and recreate it.
-if uncategorized:
-    playlist_id = uncategorized[0]['id']
+if uncategorized_lst:
+    playlist_id = uncategorized_lst[0]['id']
     api.delete_playlist(playlist_id)
 
-uncategorized_id = api.create_playlist(secret.UNCATEGORIZED)
-playlists = api.get_all_user_playlist_contents()
-uncategorized = [p for p in playlists if p['id'] == uncategorized_id]
-uncategorized = uncategorized[0]
+api.create_playlist(secret.UNCATEGORIZED)
 
-# If "Category A" playlist does not yet exist, create it
-if not category_a:
-    category_a_id = api.create_playlist(secret.CATEGORY_A)
-    playlists = api.get_all_user_playlist_contents()
-    category_a = [p for p in playlists if p['id'] == category_a_id]
-category_a = category_a[0]
+# If "Category A"playlist does not exist, create it
+if not category_a_lst:
+    api.create_playlist(secret.CATEGORY_A)
 
 # If "Category B"playlist does not exist, create it
-if not category_b:
-    category_b_id = api.create_playlist(secret.CATEGORY_B)
-    playlists = api.get_all_user_playlist_contents()
-    category_b = [p for p in playlists if p['id'] == category_b_id]
-category_b = category_b[0]
+if not category_b_lst:
+    api.create_playlist(secret.CATEGORY_B)
 
-# To complete here
+playlists = api.get_all_user_playlist_contents()
+playlists_to_use = [p for p in playlists if p['name'] in (secret.BIG_PLAYLIST, secret.UNCATEGORIZED, secret.CATEGORY_A, secret.CATEGORY_B)]
+big_playlist = [p for p in playlists_to_use if p['name'] == secret.BIG_PLAYLIST][0]
+uncategorized = [p for p in playlists_to_use if p['name'] == secret.UNCATEGORIZED][0]
+category_a = [p for p in playlists_to_use if p['name'] == secret.CATEGORY_A][0]
+category_b = [p for p in playlists_to_use if p['name'] == secret.CATEGORY_B][0]
+
+# Create sets of song ids
+all_songs_set = {song['trackId'] for song in big_playlist['tracks']}
+cat_a_songs = {song['trackId'] for song in category_a['tracks']}
+cat_b_songs = {song['trackId'] for song in category_b['tracks']}
+
+# Set of all categorized songs
+categorized_songs = cat_a_songs | cat_b_songs
+uncategorized_songs = all_songs_set - categorized_songs
+uncategorized_songs = list(uncategorized_songs)
+
+uncategorized_id = uncategorized['id']
+api.add_songs_to_playlist(uncategorized_id, uncategorized_songs)
